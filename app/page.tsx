93eb07269d27f14d3e37eb63c7093e6a42b65a28
Manar-97 <Manar-97@github.com>
2026-05-21 00:25:32 +0300

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -224,7 +224,7 @@ export default function WeddingLandingPage() {
               const text = `💍 RSVP\n\nName: ${name}\nMessage: ${message}`;
 
               window.open(
-                `https://wa.me/201021319988?text=${encodeURIComponent(text)}`,
+                `https://wa.me/201000544642?text=${encodeURIComponent(text)}`,
                 "_blank"
               );
             }}
